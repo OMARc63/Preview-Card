805595e609a1282053d95311296a2100425bb58f
Omar <abobakr072221@gmail.com>
2022-06-24 17:09:21 +0100

    completed code

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -10,21 +10,21 @@ function Card() {
           <img src={img}/>
         </div>
         <div className="preview">
-          <h2 className="title">
+          <h1 className="title">
             Get <span>insights</span> that help your business grow.
-          </h2>
-          <p>discover the benefits of data analytics and make better decisions regarding the revenue, customer experience, and the overall efficiency.</p>
+          </h1>
+          <p>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and the overall efficiency.</p>
           <div className="stats">
             <div className="stat">
-              <h4>10K+</h4>
+              <h2>10k+</h2>
               <p>Companies</p>
             </div>
             <div className="stat">
-              <h4>314</h4>
+              <h2>314</h2>
               <p>Templates</p>
             </div>
             <div className="stat">
-              <h4>12M+</h4>
+              <h2>12M+</h2>
               <p>Queries</p>
             </div>
           </div>
